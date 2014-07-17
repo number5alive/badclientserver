1cf994c4906083a77367007ad03fe79e977e154b
Steve <number5alive@gmail.com>
2014-07-17 17:47:49 -0400

    client code now uses the parsed args

--- badclientserver.c
+++ badclientserver.c
@@ -23,7 +23,6 @@ int clientmain(Arguments *pArgs)
 {
     int sockfd = 0,n = 0;
     char recvBuff[1024];
-    struct sockaddr_in serv_addr;
 
     memset(recvBuff, '0' ,sizeof(recvBuff));
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0))< 0)
@@ -32,11 +31,7 @@ int clientmain(Arguments *pArgs)
         return 1;
     }
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(5000);
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))<0)
+    if(connect(sockfd, (struct sockaddr *)&pArgs->serv_addr, sizeof(pArgs->serv_addr))<0)
     {
         printf("\n Error : Connect Failed \n");
         return 1;
